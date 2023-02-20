ef40079ab48891f3daf2c7a27169189f9c95296d
권선택 <125417038+saintlucias@users.noreply.github.com>
2023-02-20 16:46:41 +0900

    Delete ProjectGitTest directory

--- ProjectGitTest/src/main/java/com/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("첫 번째 푸시");
-		System.out.println("두 번째 푸시");
-		System.out.println("세 번째 푸시");
-	}
-}
--- ProjectGitTest/src/main/java/com/test/tttt.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class tttt {
-	public static void main(String[] args) {
-		System.out.println("잉");
-		System.out.println("혀노붐따우~");
-	}
-}
