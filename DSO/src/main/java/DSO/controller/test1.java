4b800254c58d545795e001f3ef03c7442eba0e23
name <saintlucias@naver.com>
2023-02-21 14:41:06 +0900

    커밋

---
+++ DSO/src/main/java/DSO/controller/test1.java
@@ -0,0 +1,24 @@
+package DSO.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test1
+ */
+@WebServlet(name = "test2", urlPatterns = { "/test2" })
+public class test1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+	}
+
+}
