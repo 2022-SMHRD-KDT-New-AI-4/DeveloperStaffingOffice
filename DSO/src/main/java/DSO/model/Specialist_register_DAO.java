8529188aa88ac409a0af338718927af8ad7ea47e
smhrd <smhrd@DESKTOP-OS4I19M>
2023-02-22 14:36:44 +0900

    로그인기능 커밋 (세종)

--- DSO/src/main/java/DSO/model/Specialist_register_DAO.java
+++ DSO/src/main/java/DSO/model/Specialist_register_DAO.java
@@ -15,6 +15,16 @@ public class Specialist_register_DAO {
 		return cnt;
 	}
 
+	public Specialist_register_VO specialistlogin(Specialist_register_VO vo) {
+		SqlSession session = sqlSessionFactory.openSession(true);
+		// 하나의 값만 확인하고 반환
+		// -> DB 값을 확인했을 때 같은 값이 여러개이면 오류발생!!!
+		Specialist_register_VO lvo = session.selectOne("login", vo);
+		session.close(); 	// sql 호출 마무리 되면 세션도 마무리
+							// 자원이 계속해서 사용되기 때문에 꼭 close 해줘야함
+		return lvo;
+	}
+
 	
 
 
