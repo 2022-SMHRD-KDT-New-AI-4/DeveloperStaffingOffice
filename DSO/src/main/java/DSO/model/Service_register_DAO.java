bbad6bf5e12254531fd00d41c7b02ac4f580d34c
smhrd <smhrd@218.149.140.39>
2023-02-27 11:57:10 +0900

    커밋

--- DSO/src/main/java/DSO/model/Service_register_DAO.java
+++ DSO/src/main/java/DSO/model/Service_register_DAO.java
@@ -1,5 +1,8 @@
 package DSO.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -7,7 +10,16 @@ import DSO.db.SqlSessionManager;
 
 // 상품 게시물 등록 DAO
 public class Service_register_DAO {
-	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	
+	private SqlSessionFactory sqlSessionFactory = DSO.db.SqlSessionManager.getSqlSession();
+
+	public ArrayList<Service_register_VO> selectAllserviceimg(){
+		SqlSession session = sqlSessionFactory.openSession(true);
+		List<Service_register_VO> list = session.selectList("selectAllserviceimg");
+		session.close();
+		return (ArrayList<Service_register_VO>)list;
+	}
+	
 	
 	public int serviceregister(Service_register_VO vo) {
 		SqlSession session = sqlSessionFactory.openSession(true);
